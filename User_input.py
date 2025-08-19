0f2c584204f90a177bbff94f31b4f484c09f0a8d
Krish-Naidu <krish.28.naidu@gmail.com>
2025-08-19 09:57:16 +0100

    new

---
+++ User_input.py
@@ -0,0 +1,17 @@
+# print("Enter your name:")
+# name = input()
+# print(f"Hello {name}")
+
+
+
+y = True
+while y == True:
+  x = input("Enter a number:")
+  x = float(x)
+
+#   try:
+#     y = False
+#   except:
+#     print("Wrong input, please try again.")
+
+print("Thank you!")
