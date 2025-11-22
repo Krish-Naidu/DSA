ea65e05ed3e708aeb30b3f918b07c5eebaac3c89
Krish-Naidu <krish.28.naidu@gmail.com>
2025-11-22 19:05:47 +0000

    ok

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -4,25 +4,21 @@
 # Space Complexity: O(1) - In-place sorting
 
 # Test array with unsorted numbers
-mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+mylist = [1064, 34, 25, 12, 22, 11, 90, 955]
 
 # Get the length of the array
-n = len(mylist)
-
-# Outer loop: controls number of passes
-# We need (n-1) passes to sort n elements
-for i in range(n-1):
-  
-  # Inner loop: compares adjacent elements
-  # After each pass, the largest element "bubbles up" to the end
-  # So we reduce the range by i (already sorted elements)
-  for j in range(n-i-1):
-    
-    # Compare adjacent elements
-    if mylist[j] > mylist[j+1]:
-      # Swap elements if they are in wrong order
-      # Python swap: left side gets right side values
-      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
-
-# Print the sorted array
-print("Sorted array:", mylist)
+length = len(mylist)
+#swapped = True
+while length > 0: #and swapped == True:
+   # swapped = False
+    # reduce the number of comparasions by 1 every iteration  
+    length = length - 1
+    # compare and swap 
+    for index in range(0, length):
+        first_element = mylist[index]
+        second_element = mylist[index + 1]
+        if first_element > second_element:
+            mylist[index] = second_element
+            mylist[index + 1] = first_element
+            #swapped = True
+print(mylist)
